ea2d909c5dab85033042199a53f361ea6976115e
lilin <13550935783.163.com>
2019-05-15 08:55:48 +0800

    文件与异常（去注释）： 1.读：open('name.后缀','r')，文件不存在时发生异常(图片读'rb') 2.写：open('name.后缀','w')，文件不存在时新建（图片写'wb'） 3.保存数据：文本和图片可以直接写入保存，数据结构（字典）需转换成JSON格式来保存 4.异常：try except finally close() with as

--- days/day11.py
+++ days/day11.py
@@ -1,89 +1,89 @@
-# '''
-# 文件与异常
-# '''
-# def main():
-#     f = None
-#     try:#标记可能出现异常的位置
-#         f = open('test.txt','r')#r为只读文件，文件不存在时，无法open
-#         print(f.read())
-#     except:#对异常的处理
-#         print('test fail')
-#     finally:#无论是否发生异常都执行
-#         if f:
-#             f.close()#保存并关闭文件，释放外部资源
-# if __name__ == "__main__":
-#     main()
-#
-# import time
-# def main():# 执行with操作并将操作的返回值赋给f，再关闭文件
-#     with open('test.txt','r') as f:#必须加上文件的后缀
-#         print(f.read())#f只能执行一次操作
-#     with open('test.txt','r') as f:
-#         for line in f:
-#             print(line,end = '')
-#             time.sleep(0.5)
-#     print()
-#     with open('test.txt','r') as f:
-#         lines = f.readlines()#将文件的每行信息以字符串的方式组成一个list赋给lines
-#         print(lines)
-# if __name__ == '__main__':
-#     main()
+'''
+文件与异常
+'''
+def main():
+    f = None
+    try:#标记可能出现异常的位置
+        f = open('test.txt','r')#r为只读文件，文件不存在时，无法open
+        print(f.read())
+    except:#对异常的处理
+        print('test fail')
+    finally:#无论是否发生异常都执行
+        if f:
+            f.close()#保存并关闭文件，释放外部资源
+if __name__ == "__main__":
+    main()
 
-# '''
-# 写文件：open('test.txt'.'w'),文件不存在时会自动创建一个文件
-# '''
-# from math import sqrt
-# def is_prim(n):
-#     assert n > 0
-#     for factor in range(2,int(sqrt(n) + 1 )):
-#         if n % factor == 0:
-#             return False
-#     return True if n !=1 else False
-# def main():
-#     filenames = ['a.txt','b.txt','c.txt']
-#     fs_list = []
-#     try:
-#         for filename in filenames:
-#             fs_list.append(open(filename,'w'))
-#         for num in range(1,10000):
-#             if is_prim(num):
-#                 if num < 100:
-#                     fs_list[0].write(str(num) + '\n')
-#                 elif num < 1000:
-#                     fs_list[1].write(str(num) + '\n')
-#                 else:
-#                     fs_list[2].write(str(num) + '\n')
-#     except IOError as ex:
-#         print(ex)
-#         print('writting fail!')
-#     finally:
-#         for fs in fs_list:
-#             fs.close()
-#     print('Writting succes! Let me see the file')
-#     p1 = open('a.txt')
-#     p2 = open('b.txt')
-#     p3 = open('c.txt')
-#     print(p1.readlines())
-#     print('-----------------------------------------------------')
-#     print(p2.readlines())
-#     print('-----------------------------------------------------')
-#     print(p3.readlines())
-# if __name__ == '__main__':
-#     main()
-# '''
-# 读写二进制文件
-# '''
-# def main():
-#     try:
-#         with open('诗.png','rb') as fs1:#读写二进制图片文件时，open方式为'rb'、'wb'
-#             date = fs1.read()
-#             print(type(date))
-#         with open('喜欢2.png','wb') as fs2:
-#             fs2.write(date)
-#     except:
-#         print('读写出错！')
-# if __name__ == '__main__':
-#     main()
+import time
+def main():# 执行with操作并将操作的返回值赋给f，再关闭文件
+    with open('test.txt','r') as f:#必须加上文件的后缀
+        print(f.read())#f只能执行一次操作
+    with open('test.txt','r') as f:
+        for line in f:
+            print(line,end = '')
+            time.sleep(0.5)
+    print()
+    with open('test.txt','r') as f:
+        lines = f.readlines()#将文件的每行信息以字符串的方式组成一个list赋给lines
+        print(lines)
+if __name__ == '__main__':
+    main()
+
+'''
+写文件：open('test.txt'.'w'),文件不存在时会自动创建一个文件
+'''
+from math import sqrt
+def is_prim(n):
+    assert n > 0
+    for factor in range(2,int(sqrt(n) + 1 )):
+        if n % factor == 0:
+            return False
+    return True if n !=1 else False
+def main():
+    filenames = ['a.txt','b.txt','c.txt']
+    fs_list = []
+    try:
+        for filename in filenames:
+            fs_list.append(open(filename,'w'))
+        for num in range(1,10000):
+            if is_prim(num):
+                if num < 100:
+                    fs_list[0].write(str(num) + '\n')
+                elif num < 1000:
+                    fs_list[1].write(str(num) + '\n')
+                else:
+                    fs_list[2].write(str(num) + '\n')
+    except IOError as ex:
+        print(ex)
+        print('writting fail!')
+    finally:
+        for fs in fs_list:
+            fs.close()
+    print('Writting succes! Let me see the file')
+    p1 = open('a.txt')
+    p2 = open('b.txt')
+    p3 = open('c.txt')
+    print(p1.readlines())
+    print('-----------------------------------------------------')
+    print(p2.readlines())
+    print('-----------------------------------------------------')
+    print(p3.readlines())
+if __name__ == '__main__':
+    main()
+'''
+读写二进制文件
+'''
+def main():
+    try:
+        with open('诗.png','rb') as fs1:#读写二进制图片文件时，open方式为'rb'、'wb'
+            date = fs1.read()
+            print(type(date))
+        with open('喜欢2.png','wb') as fs2:
+            fs2.write(date)
+    except:
+        print('读写出错！')
+if __name__ == '__main__':
+    main()
 '''
 JSON是纯文本,可以将数据结构转化为Json格式保存
 数据结构的序列化（数据转化为存储的字节）：
